6f03b209c47eb7958e4b791e6a23220cd29fc107
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-19 21:01:43 -0300

    Requisitos 1,2,3,4,5,6

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 const pixel = document.querySelector('#meme-image-container');
-const input = document.querySelector('#text-input');
-const button = document.querySelector('#vai');
-const buttonClear = document.querySelector('#clear');
-let div = document.createElement('div');
+let input = document.querySelector('#text-input');
+const div = document.querySelector('#meme-text');
+const memeInsert = document.querySelector('#meme-insert');
+const image = document.querySelector('#meme-image');
+pixel.style.border = 'black solid 1px';
 
-function createText() {
-  div.id = 'meme-text';
-
-  div.appendChild(document.createTextNode(input.value));
+function createText(e) {
+  div.appendChild(document.createTextNode(e.key));
   pixel.appendChild(div);
-  input.value = '';
 }
+input.addEventListener('keypress', createText);
 
-input.addEventListener('input', createText);
-
-function clearText() {
-  div.parentNode.removeChild(div);
+function addMeme() {
+  image.appendChild(memeInsert);
 }
+memeInsert.addEventListener('click', addMeme);
 
-buttonClear.addEventListener('click', clearText);
+// Bônus:
+
+const buttonFire = document.querySelector('#fire');
+const buttonWater = document.querySelector('#water');
+const buttonEarth = document.querySelector('#earth');
+
+function changeBorderFire() {
+  pixel.style.border = '3px dashed red';
+}
+buttonFire.addEventListener('click', changeBorderFire);
+
+function changeBorderWater() {
+  pixel.style.border = 'blue 5px double';
+}
+buttonWater.addEventListener('click', changeBorderWater);
+
+function changeBorderEarth() {
+  pixel.style.border = 'groove green 6px';
+}
+buttonEarth.addEventListener('click', changeBorderEarth);
+
+// Bônus:
+
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
+
+function chooseMeme1() {
+  image.src = './imgs/meme1.png';
+}
+meme1.addEventListener('click', chooseMeme1);
+
+function chooseMeme2() {
+  meme2.style.width = '500px';
+  meme2.style.height = '500px';
+  image.appendChild(meme2);
+}
+meme2.addEventListener('click', chooseMeme2);
+
+function chooseMeme3() {
+  meme3.style.width = '500px';
+  meme3.style.height = '500px';
+  image.appendChild(meme3);
+}
+meme3.addEventListener('click', chooseMeme3);
+
+function chooseMeme4() {
+  meme4.style.width = '500px';
+  meme4.style.height = '500px';
+  image.appendChild(meme4);
+}
+meme4.addEventListener('click', chooseMeme4);
